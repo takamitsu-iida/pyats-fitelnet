c522dad4cd26a656c27b128e9837f1755a70ab37
takamitsu-iida <takamitsu.iida@gmail.com>
2023-01-24 16:13:27 +0900

    minor fix

--- examples/bin/retrieve_sid.py
+++ examples/bin/retrieve_sid.py
@@ -6,18 +6,13 @@ import os
 from genie.testbed import load
 from genie.metaparser.util.exceptions import SchemaEmptyParserError
 
+# ./common.py
+import common
+
 logger = logging.getLogger(__name__)
 
 # app_home is .. from this file
 app_home = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-
-# load genie external_parser
-try:
-    from external_parser.fitelnet.common import Common
-except ImportError:
-    print('failed to import parser, please check sys.path')
-    print(sys.path)
-    sys.exit(1)
 
 # log dir
 log_dir = os.path.join(app_home, 'log')
@@ -64,8 +59,6 @@ if __name__ == '__main__':
     import argparse
     import sys
 
-    import common
-
     logging.basicConfig()
     logger.setLevel(logging.INFO)
 
@@ -100,7 +93,7 @@ if __name__ == '__main__':
         # pprint(parsed_dict)
 
         # convert to csv
-        csv_data = Common.to_csv_from_dict(parsed_dict=parsed_dict, template=TEMPLATE)
+        csv_data = common.to_csv_from_dict(parsed_dict=parsed_dict, template=TEMPLATE)
 
         # 保存するファイル名
         csv_file_path = os.path.join(log_dir, f'{os.path.basename(__file__)}.csv')
@@ -111,7 +104,7 @@ if __name__ == '__main__':
             f.close()
 
         # テーブル形式で表示
-        table_data = Common.to_tabulate_from_csv(csv_data=csv_data)
+        table_data = common.to_tabulate_from_csv(csv_data=csv_data)
         print(table_data)
 
         return 0
